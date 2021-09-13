71dfdec2a638702bcd03319a48638996fa2056b5
Hendra Nicholas <hendra@41studio.com>
2021-09-13 16:50:04 +0700

    refactor code

--- app/controllers/battings_controller.rb
+++ app/controllers/battings_controller.rb
@@ -6,9 +6,10 @@ class BattingsController < ApplicationController
     conditions = {}
     conditions[:year_id] = params[:year] if params[:year].present?
     conditions[:team_id] = params[:team_id] if params[:team_id].present?
+    sql = "*, SUM(ab) AS total_ab, SUM(h) AS total_h, GROUP_CONCAT(team_id, ',') AS tids, (CAST(SUM(h) AS float)/CAST(SUM(ab) AS float)) AS ba"
     @battings = Batting.where(conditions)
                         .group(:player_id, :year_id)
-                        .select("*, SUM(ab) AS total_ab, SUM(h) AS total_h, GROUP_CONCAT(team_id, ',') AS tids, (CAST(SUM(h) AS float)/CAST(SUM(ab) AS float)) AS ba")
+                        .select(sql)
                         .order('ba ASC')
                         .page(params[:page])
   end
